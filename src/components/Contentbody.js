b08b53a6d2511c603b546cda5334b148405ec25b
Shubhojeet Bera <shubhobera98@gmail.com>
2023-01-23 14:25:19 +0530

    updated

---
+++ src/components/Contentbody.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Contentbody() {
+  return (
+    <div className="container text-center my-5">
+        <h1>To-Do App</h1>
+        <div className="input-group input-group-lg">
+  <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg"/>
+</div>
+    </div>
+  )
+}
