22c439a92aa67ed992ade8f9b16ca0389c9c2959
Shubhojeet Bera <shubhobera98@gmail.com>
2023-01-23 18:05:34 +0530

    updated

--- src/components/Contentbody.js
+++ src/components/Contentbody.js
@@ -2,42 +2,28 @@ import React, { useState } from "react";
 import Task from "./Task";
 
 export default function Contentbody() {
-  const [tasks, settasks] = useState();
-  const [Totaltasks, setTotaltasks] = useState([]);
-  const [taskNo, settaskNo] = useState(0);
-
-  const AddTask = () => {
-    settasks(document.getElementById("main-text").value);
-    settaskNo(taskNo+1)
-    document.getElementById("main-text").value = "";
+  const [Input, setInput] = useState();
+  const InputChange = (e) => {
+    setInput(e.target.value);
   };
-
-  const DeleteTask = () => {
-    document.getElementById("main-text").value = "";
-    settasks(document.getElementById("main-text").value);
-    settaskNo(taskNo-1)
-    console.log("deleted");
-  };
-
   return (
     <>
       <div className="container text-center my-5">
         <h1>To-Do App</h1>
         <div className="input-group input-group-lg my-4">
           <input
-            id="main-text"
             type="text"
             className="form-control"
             aria-label="Sizing example input"
             placeholder="Enter your tasks here.."
             aria-describedby="inputGroup-sizing-lg"
+            onChange={InputChange}
           />
-          <button onClick={AddTask} type="button" className="btn btn-success">
+          <button type="button" className="btn btn-success">
             Add
           </button>
         </div>
-        <Task taskNo={taskNo} tasks={tasks} delete={DeleteTask}/>
-
+        <Task input={Input}/>
       </div>
     </>
   );
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,25 +1,21 @@
-import React from 'react'
+import React from "react";
 
 export default function Task(props) {
-
   return (
-  <>
-  <div className="preview my-5">
-
-          <h2>Your Tasks</h2>
-          {props.taskNo>0?(<div id='TaskCard' class="card my-5 w-50">
-            <div class="card-body">
-              <h5 class="card-title">Task #{props.taskNo}</h5>
-              <p class="card-text">
-               {props.tasks}
-              </p>
-              <a onClick={props.delete} class="btn btn-danger">
-                Delete
-              </a>
-            </div>
-          </div>):<p className="my-5">Nothing to show here!</p>}
-         
+    <>
+      <div class="col-sm-6 mb-3 mb-sm-0">
+        <div class="card text-left">
+          <div class="card-body">
+            <h5 class="card-title">Task 1</h5>
+            <p class="card-text">
+              {props.input}
+            </p>
+            <a href="/" class="btn btn-danger">
+              Delete
+            </a>
+          </div>
         </div>
-  </>
-  )
+      </div>
+    </>
+  );
 }
